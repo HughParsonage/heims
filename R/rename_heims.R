75848088afb136eb632e4741ebacbb4d669d118a
HughParsonage <hugh.parsonage@gmail.com>
2017-02-06 16:28:46 +1100

    Permit renaming vec elements, not just names(v)

--- R/rename_heims.R
+++ R/rename_heims.R
@@ -1,9 +1,13 @@
 #' Make HEIMS element nos human-readable
+#' @name element_decoders
 #' @param DT The data table with original names
-#' @return DT with the new names
+#' @param v A vector of element names.
+#' @return DT with the new names or the vector with the names translated.
 #' @details See \code{\link{heims_data_dict}}.
-#' @export
 
+
+#' @rdname element_decoders
+#' @export
 rename_heims <- function(DT){
   DTnoms <- names(DT)
   decoder <-
@@ -16,6 +20,24 @@ rename_heims <- function(DT){
            old = decoder[["orig_name"]],
            new = decoder[["long_name"]])
   DT
+}
+
+#' @rdname element_decoders
+#' @export element2name
+element2name <- function(v){
+  decoder <-
+    lapply(heims_data_dict, function(x){
+      data.table(long_name = unlist(x[names(x) == "long_name"]),
+                 v. = unlist(x[names(x) == "orig_name"]))
+    }) %>%
+    rbindlist(use.names = TRUE, fill = TRUE) %>%
+    .[v. %in% v]
+
+  decoder %>%
+    .[data.table(v. = v), on = "v."] %>%
+    # Retain the name if join unsuccessful
+    .[, long_name := if_else(is.na(long_name), v., long_name)] %>%
+    .[["long_name"]]
 }
 
 
