877e2397e2992c9a1a0296b48c91620deffd90b6
HughParsonage <hugh.parsonage@gmail.com>
2017-02-09 17:32:46 +1100

    Add past elements to E490

--- data-raw/put_decoders.R
+++ data-raw/put_decoders.R
@@ -3,6 +3,6 @@
 E490_decoder <-
   data.table::fread("./data-raw/decoders/E490-decoders.txt")
 
-devtools::use_data(E490_decoder, internal = FALSE)
+devtools::use_data(E490_decoder, internal = FALSE, overwrite = TRUE)
 
 
