92a9de3b74038c2f6d5e8206c973388f5a680d11
HughParsonage <hugh.parsonage@gmail.com>
2017-02-11 12:05:47 +1100

    Correct E347 encoding

--- tests/testthat/test_validate_elements.R
+++ tests/testthat/test_validate_elements.R
@@ -3,7 +3,9 @@ context("Validate elements")
 test_that("validate_elements returns TRUE when valid", {
   # http://heimshelp.education.gov.au/sites/heimshelp/2016_data_requirements/2016dataelements/pages/306
   # says that a value, x, in variable E306 is valid iff x = 0 or x \in (1000, 9999)
-  X <- data.frame(E306 = as.integer(c(0, 1011, 9999, 9998)))
+  X <- data.frame(E306 = as.integer(c(0, 1011, 9999, 9998, 4500)),
+                  E347 = c("0001", "A998", "1987", "1980", "2020"),
+                  stringsAsFactors = FALSE)
   expect_true(all(validate_elements(X)))
 })
 
