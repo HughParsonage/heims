596279f14d43c942df89813644897af14a2fa2e9
HughParsonage <hugh.parsonage@gmail.com>
2017-02-25 17:44:04 +1100

    Add browse_elements

---
+++ R/browse_elements.R
@@ -0,0 +1,16 @@
+#' Browse elements for description
+#' @param pattern A perl expression or expressions to match in the long name as in \code{\link{heims_data_dict}}.
+#' @export browse_elements
+
+browse_elements <- function(pattern){
+  if (length(pattern) > 1){
+    pattern <- paste0("(", paste0(pattern, collapse = ")|("), ")")
+  }
+
+  lapply(heims_data_dict, function(x){
+    data.table(long_name = unlist(x[names(x) == "long_name"]),
+               orig_name = unlist(x[names(x) == "orig_name"]))
+  }) %>%
+    rbindlist(use.names = TRUE, fill = TRUE) %>%
+    .[grepl(pattern, long_name, perl = TRUE)]
+}
