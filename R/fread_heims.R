e035fb87bc44c78d8123f3b8f387341dc54a515b
HughParsonage <hugh.parsonage@gmail.com>
2017-03-13 18:01:13 +1100

    Initial commit of fread_heims

---
+++ R/fread_heims.R
@@ -0,0 +1,18 @@
+#' Read raw heims file
+#' @param filename A text-delimeted file, passed to \code{fread} from \code{data.table}.
+#' @return A \code{data.table}.
+#' @export
+
+fread_heims <- function(filename){
+  file <- gsub("^.*((enrol)|(completions)|(load)).*$", filename, filename)
+  fread(filename,
+        na.strings = c("", "NA", "?", ".", "*", "**",
+                             # CHESSN
+                             "ZZZZZZZZZZ"),
+        # course file does not contain "E313"
+        colClasses = switch(file,
+                            "enrol" = list(character = c("E313", "E347")),
+                            "completions" = list(character = c("E313", "E347")),
+                            "load" = list(character = c("E313")),
+                            NULL))
+}
