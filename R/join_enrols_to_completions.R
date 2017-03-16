b58a944a2149758103d2ff2d0e4b6a7b9148d1f2
HughParsonage <hugh.parsonage@gmail.com>
2017-03-16 22:51:03 +1100

    Initial commit of join enrols to completions

---
+++ R/join_enrols_to_completions.R
@@ -0,0 +1,84 @@
+#' @param enrols A data.table for enrolments (right table in the right outer joins)
+#' @param enrolYearById Two-column data table: one column named the same as
+#'   \code{enrol.id} and the other representing the enrolment year of that id.
+#' @param enrol.id The unique identifier of each enrolment.
+#' @param completionYearById Two-column data table: one column named the same as
+#'   \code{completion.id} and the other representing the completion year of that id.
+#' @param completion.id The unique identifier of each completion (possibly spread
+#'   over multiple tables).
+#' @param completion.year The column in \code{completionTables} referring to the
+#'   year of completion.
+#' @param completionTables A list. Each element contains two elements,
+#'  the first, \code{DT}, is a data.table, the second is passed to the
+#'  \code{on = } argument \code{data.table}. For example
+#'  \code{c("HE_Provider_name", "Student_id", "completion_Year>=enrol_Year")}
+#'   for a non-equi join.
+#' @param debug Print intermediate tables.
+join_enrols_to_completions <- function(enrolTable,
+                                       enrolYearById,
+                                       enrol.id = "Enrol_Grattan_id",
+                                       completionYearById,
+                                       completion.id = "Completions_Grattan_id",
+                                       completion.year = "completion_Year",
+                                       completionTables,
+                                       debug = FALSE){
+
+  stopifnot(is.data.table(enrolTable),
+            is.data.table(enrolYearById),
+            enrol.id %in% names(enrolYearById),
+            enrol.id %in% names(enrolTable),
+            is.integer(enrolYearById %>% select_(.dots = setdiff(names(enrolYearById), enrol.id)) %>% .[[1]]),
+            is.list(completionTables))
+
+  check_completionTable <- function(el){
+    stopifnot(length(el) == 2L,
+              is.data.table(el[[1]]),
+              is.character(el[[2]]))
+    invisible(NULL)
+  }
+
+  invisible(lapply(completionTables, check_completionTable))
+
+  the_enrolYearById <- enrolYearById[enrolTable, .SD, .SDcols = names(enrolYearById), on = enrol.id]
+
+  # The join is intended to keep only those
+  # with Year greater in the completion file than in the enrol file
+  join_two <- function(enrol, complete, on_key){
+    complete[enrol, on = on_key] %>%
+      .[the_enrolYearById, on = enrol.id] %>%
+      # At this point "completion.year" is in fact the enrol year
+      .[, (completion.year) := NULL] %>%
+      completionYearById[., on = completion.id]
+  }
+
+  out <- copy(enrolTable)
+
+  for (i in seq_along(completionTables)){
+    completions_tbl <- copy(completionTables[[i]][[1]])
+    # Do not attempt match if match already made
+    if (i == 1L){
+      out <- join_two(out, completions_tbl, on_key = c(completionTables[[i]][[2]]))
+      out[, .no_match := is.na(.SD[[completion.year]])]
+      if (debug){
+        cat("First join:")
+        print(out)
+        cat('\n\n\n\n')
+      }
+    } else {
+      completions_tbl[, .no_match := TRUE]
+      out <- join_two(out, completions_tbl, on_key = c(".no_match", completionTables[[i]][[2]]))
+      if (debug){
+        cat("\n\n\n\n ", i, "\n")
+        print(out)
+      }
+      out[, .no_match := and(is.na(out[[completion.year]]), out[[".no_match"]])]
+      if (paste0("i.", completion.year) %in% names(out)){
+        out[, (completion.year) := coalesce(out[[paste0("i.", completion.year)]], out[[completion.year]])]
+        out[, (paste0("i.", completion.year)) := NULL]
+      }
+    }
+  }
+
+  out[, .(enrol.id, completion.year)]
+}
+
---
+++ tests/testthat/test_join_enrols_to_completions.R
@@ -0,0 +1,46 @@
+test_that("Joined known example", {
+
+  # a la enrols
+  enrols <-
+    data.table(HE_Provider_name = c("a", "a", "a", "b", "b", "c", "c", "e"),
+               Student_id = 1:8,
+               CHESSN = 11:18,
+               Enrol_Grattan_id = LETTERS[1:8],
+               enrol_Year = as.integer(c(2005, 2006, 2007, 2010, 2008, 2009, 2010, 2011)))
+
+  enrol_Year_by_id <-
+    enrols %>%
+    select(Enrol_Grattan_id, enrol_Year)
+
+  completions <-
+    data.table(HE_Provider_name = c("a", "a", "b", "b", "c", "c"),
+               Student_id = c(1, 2, 1, 2, 1, 2),
+               CHESSN = c(11:16),
+               Completions_Grattan_id = LETTERS[11:16],
+               completion_Year = as.integer(c(2006, 2006, 2006, 2010, 2005, 2004)))
+
+  completion_Year_by_id <-
+    completions %>%
+    select(Completions_Grattan_id, completion_Year)
+
+  # a la completions
+  completions_by_Student_id <-
+    completions %>%
+    select(HE_Provider_name, Student_id, Completions_Grattan_id, completion_Year)
+
+  completions_by_CHESSN <-
+    completions %>%
+    select(CHESSN, Completions_Grattan_id, completion_Year)
+
+  expect_identical(join_enrols_to_completions(enrolTable = enrols,
+                                              enrolYearById = enrol_Year_by_id,
+                                              enrol.id = "Enrol_Grattan_id",
+                                              completionYearById = completion_Year_by_id,
+                                              completion.id = "Completions_Grattan_id",
+                                              completionTables = list(list(DT = completions_by_Student_id,
+                                                                           on_key = c("HE_Provider_name", "Student_id", "completion_Year>=enrol_Year")),
+                                                                      list(DT = completions_by_CHESSN,
+                                                                           on_key = c("CHESSN", "completion_Year>=enrol_Year")))),
+                   data.table(Enrol_Grattan_id = LETTERS[1:8],
+                              completion_Year = as.integer(c(2006, 2006, NA, 2010, NA, NA, NA, NA))))
+})
