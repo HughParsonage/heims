8571a7dbb4b56937aa6ebc50d02e023d4f445a38
HughParsonage <hugh.parsonage@gmail.com>
2017-04-23 11:55:10 +1000

    Needs to be integer for validation in decoder

--- tests/testthat/test_TableA.R
+++ tests/testthat/test_TableA.R
@@ -2,5 +2,5 @@ context("Table A")
 
 test_that("Table A providers even if retired", {
   # University of Tasmania
-  expect_true(decode_heims(data.table(E306 = 1052))[["TableA"]])
+  expect_true(decode_heims(data.table(E306 = 1052L))[["TableA"]])
 })
